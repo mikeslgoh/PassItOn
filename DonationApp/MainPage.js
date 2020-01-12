2a7f16437eca7193b18940cead593330cd5b0c7a
William Beltran <williambeltran@Williams-MacBook-Air-3.local>
2020-01-11 19:06:00 -0800

    Search Bar Getting started

---
+++ DonationApp/MainPage.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import { SearchBar } from 'react-native-elements';
+
+
+
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+
+export default class MainPage extends React.Component {
+  state = {
+    search: '',
+  };
+
+  updateSearch = search =>{
+    this.setState({search});
+  };
+
+  render() {
+    const { search } = this.state;
+    let filteredOrganizations = this.props.organizations.filter(
+      (organization) => {
+        return // Condition where we return organization
+      }
+    );
+    return (
+      <>
+      <StatusBar barStyle="dark-content" />
+          <ScrollView
+            contentInsetAdjustmentBehavior="automatic"
+            style={styles.scrollView}>
+            <Header />
+            {global.HermesInternal == null ? null : (
+              <View style={styles.engine}>
+                <Text style={styles.footer}>Engine: Hermes</Text>
+              </View>
+            )}
+            <View style={styles.body}>
+              <View style={styles.sectionContainer}>
+                <SearchBar
+                  placeholder="Type Here..."
+                  onChangeText={this.updateSearch}
+                  value={search}
+                />
+              </View>
+            </View>
+          </ScrollView>
+      </>
+    );
+  }
+}
+
+
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
